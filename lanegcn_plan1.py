990f3b4a8e6e023dbb25099b83b1a4f61d6e8995
kingwmk <1171147450@qq.com>
2021-03-08 12:09:55 +0800

    Update lanegcn_plan1.py

--- lanegcn_plan1.py
+++ lanegcn_plan1.py
@@ -160,11 +160,11 @@ class ActorNet_P1(nn.Module):
         # -> Mx20xn_out
         actors = self.relu(self.inputLayer(actors))
         print("actors"+str(actors.shape))
-        print("actor_idcs"+str(actor_idcs.size()))
-        print("actor_ctrs"+str(actor_ctrs.size()))
+        print("actor_idcs"+str(len(actor_idcs))+str(actor_idcs[0].shape))
+        print("actor_ctrs"+str(len(actor_ctrs))+str(actor_ctrs[0].shape))
         print("nodes"+str(nodes.shape))
-        print("nodes_idcs"+str(node_idcs.size()))
-        print("node_ctrs"+str(node_ctrs.size()))
+        print("nodes_idcs"+str(len(node_idcs))+str(node_idcs[0].shape))
+        print("node_ctrs"+str(len(node_ctrs))+str(node_ctrs.size[0].shape))
 #        actors = self.m2a(actors, actor_idcs, actor_ctrs, nodes, node_idcs, node_ctrs)
    
 #        lstm_state = self.cell.forward(actors, (hidden_states_current,cell_states_current))
